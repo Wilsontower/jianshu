ebedbe22c9d57701f73e0b7bd1caf1ac647483d9
wangtao <2016302580147@whu.edu.cn>
2019-05-11 17:38:09 +0800

    change bookOrder to Order

--- src/main/java/com/whu/jianshu/service/OrderService.java
+++ src/main/java/com/whu/jianshu/service/OrderService.java
@@ -1,6 +1,6 @@
 package com.whu.jianshu.service;
 
-import com.whu.jianshu.entity.order.BookOrder;
+import com.whu.jianshu.entity.order.Order;
 import com.whu.jianshu.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,20 +12,20 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-    public BookOrder addNewOrder(BookOrder bookOrder){return orderRepository.save(bookOrder);}
+    public Order addNewOrder(Order Order){return orderRepository.save(Order);}
     public void deleteOrder(String orderID) {
         orderRepository.deleteById(orderID);
     }
 
-    public BookOrder getOrderById(String orderID) {
+    public Order getOrderById(String orderID) {
         return orderRepository.findByOrderIDIs(orderID);
     }
 
-    public BookOrder updateOrder(BookOrder order) {
+    public Order updateOrder(Order order) {
         return orderRepository.saveAndFlush(order);
     }
 
-    public List<BookOrder> getAllOrderByUserId(String userId) {
+    public List<Order> getAllOrderByUserId(String userId) {
         return orderRepository.findByUserIDIs(userId);
     }
 }
