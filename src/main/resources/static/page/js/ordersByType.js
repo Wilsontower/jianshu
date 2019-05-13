af0566c6c2a0ad8600de7941f8f65f1bf7427344
wt <929306539@qq.com>
2019-05-13 21:53:16 +0800

    change the way to show orders

--- src/main/resources/static/page/js/ordersByType.js
+++ src/main/resources/static/page/js/ordersByType.js
@@ -1,6 +1,6 @@
 var INDEX = angular.module("jianshuApp");
 // 主页控制器
-INDEX.controller("mainCtrl", ['$scope', '$http', '$state', function ($scope, $http, $state) {
+INDEX.controller("obtCtrl", ['$scope', '$http', '$state', function ($scope, $http, $state) {
 
     $scope.enterDetails = function () {
         $state.go("details");
